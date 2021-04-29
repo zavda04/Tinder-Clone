b9a91530787234124566bdff7bd5f3f13c15bfe7
thomas deveza <thomasdeveza@iMac-de-thomas.local>
2021-04-29 11:37:05 +0200

    init routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import './App.css';
-import Header from './Header';
+import "./App.css";
+import Header from "./Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Tinder Clone</h1>
-
       {/* HEADER */}
       <Header />
 
-      
-      {/* TINDER CARDS */}
-      {/* Buttons below tinde cards  */}
-
-      {/* Chats screen  */}
-      {/* Individual chat screen  */}
+      <Router>
+        <Switch>
+          <Route path="/chat">
+            <h1>ChatPage</h1>
+          </Route>
+          <Route path="/">
+            <h1>HomePage</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+{
+  /* TINDER CARDS */
+}
+{
+  /* Buttons below tinde cards  */
+}
+
+{
+  /* Chats screen  */
+}
+{
+  /* Individual chat screen  */
+}
