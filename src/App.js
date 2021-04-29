b79711737a3028bbe427b5da6e10445842e6b79a
thomas deveza <thomasdeveza@iMac-de-thomas.local>
2021-04-29 11:21:02 +0200

    Build Header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tinder Clone</h1>
+
+      {/* HEADER */}
+      <Header />
+
+      
+      {/* TINDER CARDS */}
+      {/* Buttons below tinde cards  */}
+
+      {/* Chats screen  */}
+      {/* Individual chat screen  */}
     </div>
   );
 }
