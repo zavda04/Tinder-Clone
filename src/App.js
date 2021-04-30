442b344fbfb4f09183f4387a0acc3a9f5ef8c7df
thomas deveza <thomasdeveza@iMac-de-thomas.local>
2021-04-30 11:26:30 +0200

    SwipeButtons Front-End

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./Header";
 import TinderCards from './TinderCards';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SwipeButtons from "./SwipeButtons";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           </Route>
           <Route path="/">
             <TinderCards />
+            <SwipeButtons />
           </Route>
         </Switch>
       </Router>
--- src/TinderCards.js
+++ src/TinderCards.js
@@ -7,14 +7,17 @@ function TinderCards() {
   const [people, setPeople] = useState([]);
 
   useEffect(() => {
-    database.collection("people").onSnapshot((snapshot) => {
+    const unsubscribe = database.collection("people").onSnapshot((snapshot) => {
       setPeople(snapshot.docs.map((doc) => doc.data()));
     });
+
+    return () => {
+      unsubscribe();
+    };
   }, []);
 
   return (
     <div>
-      {/* <h1>Tinder Cards</h1> */}
       <div className="tinderCards__cardContainer">
         {people.map((person) => (
           <TinderCard
